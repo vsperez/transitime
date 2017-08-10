02c3ca04ea8a42c4eb5fc412255672a4eb1a623a
scrudden <og.crudden@gmail.com>
2017-08-10 20:27:12 +0100

    Adding fast playback prediction accuracy module.

--- transitime/src/main/java/org/transitime/avl/PlaybackModule.java
+++ transitime/src/main/java/org/transitime/avl/PlaybackModule.java
@@ -16,12 +16,17 @@
  */
 package org.transitime.avl;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.transitime.applications.Core;
+import org.transitime.config.BooleanConfigValue;
 import org.transitime.config.StringConfigValue;
 import org.transitime.core.AvlProcessor;
 import org.transitime.db.structs.AvlReport;
@@ -61,8 +66,16 @@ public class PlaybackModule extends Module {
 			new StringConfigValue("transitime.avl.playbackStartTime", 
 					"",
 					"Date and time of when to start the playback.");
-
 	
+	private static StringConfigValue playbackEndTimeStr =
+			new StringConfigValue("transitime.avl.playbackEndTime", 
+					"",
+					"Date and time of when to end the playback.");
+
+	private static BooleanConfigValue playbackRealtime =
+			new BooleanConfigValue("transitime.avl.playbackRealtime", 
+					false,
+					"Playback at normal time speed rather than as fast as possible.");
 	/********************* Logging **************************/
 	private static final Logger logger = 
 			LoggerFactory.getLogger(PlaybackModule.class);
@@ -81,8 +94,7 @@ public class PlaybackModule extends Module {
 				|| getPlaybackVehicleId().isEmpty()
 				|| getPlaybackStartTimeStr() == null
 				|| getPlaybackStartTimeStr().isEmpty()) {
-			System.err.println("Parameters not set. See log file for details. Exiting.");
-			System.exit(-1);
+			logger.warn("Parameters not set. See log file for details. Exiting.");			
 		}
 		
 		// Initialize the dbReadBeingTime member
@@ -115,7 +127,32 @@ public class PlaybackModule extends Module {
 			return -1;
 		}
 	}
-	
+	private static long parsePlaybackEndTime(String playbackEndTimeStr) {
+		try {
+			long playbackEndTime = Time.parse(playbackEndTimeStr).getTime();
+			
+			// If specified time is in the future then reject.
+			if (playbackEndTime > System.currentTimeMillis()) {
+				logger.error("Playback end time \"{}\" specified by " +
+						"transitime.avl.playbackEndTime parameter is in " +
+						"the future and therefore invalid!",
+						playbackEndTimeStr);
+				System.exit(-1);					
+			}
+				
+			return playbackEndTime;
+		} catch (java.text.ParseException e) {
+			logger.error("Paramater -t \"{}\" specified by " +
+					"transitime.avl.playbackEndTime parameter could not " +
+					"be parsed. Format must be \"MM-dd-yyyy HH:mm:ss\"",
+					playbackEndTimeStr);
+			System.exit(-1);
+			
+			// Will never be reached because the above state exits program but
+			// needed so compiler doesn't complain.
+			return -1;
+		}
+	}
 	/**
 	 * Gets a batch of AVl data from the database
 	 * @return
@@ -141,7 +178,7 @@ public class PlaybackModule extends Module {
 		
 		logger.info("PlaybackModule read {} AVLReports.", avlReports.size());
 
-		// For next time this method is called
+		// For next time this method is called.
 		dbReadBeginTime = end;
 		
 		// Return results
@@ -154,26 +191,68 @@ public class PlaybackModule extends Module {
 	 */
 	@Override
 	public void run() {
-		// Keep running as long as not trying to access in the future.
-		while (dbReadBeginTime < System.currentTimeMillis()) {
+		// Keep running as long as not trying to access in the future.		
+		while (dbReadBeginTime < System.currentTimeMillis() && (playbackEndTimeStr.getValue().length()==0 || dbReadBeginTime<parsePlaybackEndTime(playbackEndTimeStr.getValue()))) {
 			List<AvlReport> avlReports = getBatchOfAvlReportsFromDb();
 			
 			// Process the AVL Reports read in.
+			long last_avl_time=-1;
 			for (AvlReport avlReport : avlReports) {
 				logger.info("Processing avlReport={}", avlReport);
 				
 				// Update the Core SystemTime to use this AVL time
 				Core.getInstance().setSystemTime(avlReport.getTime());
 				
+				DateFormat estFormat = new SimpleDateFormat("yyyyMMddHHmmss");				
+		        TimeZone estTime = TimeZone.getTimeZone("EST");
+		        estFormat.setTimeZone(estTime);
+		        
+		        TimeZone gmtTime = TimeZone.getTimeZone("GMT");		        
+		        DateFormat gmtFormat =  new SimpleDateFormat("yyyyMMddHHmmss");
+		        gmtFormat.setTimeZone(gmtTime);
+		        		    		        		       
+		        String estDate=estFormat.format(avlReport.getTime());
+		        String gmtDate=gmtFormat.format(avlReport.getTime());		        
+		        		     
+		        
+				if(playbackRealtime.getValue()==true)
+				{
+					if(last_avl_time>-1)
+					{
+						try {
+							// only sleep if values less than 10 minutes. This is to allow it skip days/hours of missing data.
+							if((avlReport.getTime()-last_avl_time)<600000)
+							{								
+								Thread.sleep(avlReport.getTime()-last_avl_time);
+							}
+							last_avl_time=avlReport.getTime();
+						} catch (InterruptedException e) {							
+							e.printStackTrace();
+						}
+					}else
+					{
+						last_avl_time=avlReport.getTime();
+					}
+				}				
 				// Do the actual processing of the AVL data
-				AvlProcessor.getInstance().processAvlReport(avlReport);
+				AvlProcessor.getInstance().processAvlReport(avlReport);						
+			
+			}
+		}
+		// Wait for database queue to be emptied before exiting.
+		while(Core.getInstance().getDbLogger().queueSize()>0)
+		{
+			try {
+				Thread.sleep(10000);
+			} catch (InterruptedException e) {
+			
 			}
 		}
 		
-		logger.info("Read in AVL in playback mode all the way up to current " +
+ 		logger.info("Read in AVL in playback mode all the way up to current " +
 				"time so done. Exiting.");
-		System.exit(0);
 		
+		System.exit(0);	
 	}
 	
 }
